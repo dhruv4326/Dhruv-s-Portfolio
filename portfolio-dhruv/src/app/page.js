c3c3eb789de7ca8a5eb46951868bf358b4c62f23
dhruv4326 <dhruvs2645@gmail.com>
2024-11-24 01:49:50 +0530

    Updated project structure and fixed app/pages issues

--- portfolio-dhruv/src/app/page.js
+++ portfolio-dhruv/src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
-import Technologies from "./components/Technologies";
-import AboutSection from "./components/AboutSection"
-import ProjectSection from "./components/ProjectSection";
-import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import Technologies from "../components/Technologies";
+import AboutSection from "../components/AboutSection"
+import ProjectSection from "../components/ProjectSection";
+import EmailSection from "../components/EmailSection";
+import Footer from "../components/Footer";
 
 
 
